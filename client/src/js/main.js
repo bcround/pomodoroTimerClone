4928043e3bbf2b06012ed6e3eae9151e4242ce5c
iamkjw77 <iamkjw77@naver.com>
2021-01-21 03:02:41 +0900

    feat: make addTask
    closed: #15

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,11 +1,29 @@
+import foo from './addTodo';
+
 export default {
   tasks: [
-    // {
-    //   id: 1,
-    //   content: 'html',
-    //   completed: true,
-    //   pomodoro: 0,
-    //   note: ''
-    // }
+    {
+      id: 1,
+      content: 'html',
+      completed: true,
+      pomodoro: 0,
+      note: ''
+    },
+    {
+      id: 2,
+      content: 'css',
+      completed: false,
+      pomodoro: 0,
+      note: ''
+    },
+    {
+      id: 3,
+      content: 'js',
+      completed: true,
+      pomodoro: 0,
+      note: ''
+    }
   ]
 };
+
+foo();
