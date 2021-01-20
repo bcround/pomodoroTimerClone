f027531887a04091f5dbf2e76c0779af9bbefe46
bg-shorthand <hanhanhan333@gmail.com>
2021-01-20 19:41:20 +0900

    go js

--- client/src/js/main.js
+++ client/src/js/main.js
@@ -1,12 +1,11 @@
-import { foo } from './addTask';
-import upAndDown from './estPomo';
-import test from './_add_task_render';
-
-export let state = {
-  task: "",
-  count: 0
+export default {
+  tasks: [
+    // {
+    //   id: 1,
+    //   content: 'html',
+    //   completed: true,
+    //   pomodoro: 0,
+    //   note: ''
+    // }
+  ]
 };
-
-upAndDown();
-foo();
-test();
