73478be23338d7de5eac8993b17c589ade441cea
bcround <sbc1142@gmail.com>
2021-01-20 22:44:30 +0900

    feat: Add rendering task javascript and correct some css in todolist

---
+++ client/src/js/taskRender.js
@@ -0,0 +1,18 @@
+import base from './main';
+
+export default function render() {
+  // DOM
+  const $todoList = document.querySelector('.todolist__list');
+  let html = '';
+
+  base.tasks.forEach(({ id, content, completed, pomodoro }) => {
+    html += `<li class="list__item" id=${id}>
+    <input class="list__checkbox" type="checkbox" ${completed ? 'checked' : ''}>
+    <span class="list__title">${content}</span>
+    <span class="list__repeat">0/${pomodoro}</span>
+    <button class="list__btn"></button>
+  </li>`;
+  });
+
+  $todoList.innerHTML = html;
+}
