6c7170fa5c943d54f3d46e0257c42fd0c3f56574
bg-shorthand <hanhanhan333@gmail.com>
2021-01-21 16:26:35 +0900

    feat: timer countdown

--- client/src/js/timer/selectMode.js
+++ client/src/js/timer/selectMode.js
@@ -1,13 +1,23 @@
-import timerState from './timerState';
-
-let { state } = timerState;
+import { timerState, time } from './timerState';
 
 const $body = document.querySelector('body');
 const $timerStart = document.querySelector('label[for="timerStart"]');
+const $timerCountdown = document.querySelector('.timer__countdown');
+const $timerMenu = document.querySelector('.timer__menu');
 
-export default () => {
-  const $timerMenu = document.querySelector('.timer__menu');
+export const exportTime = {
+  min: time.pomodoroMin,
+  sec: 0
+};
 
+// let min = time.pomodoroMin;
+// let sec = 0;
+
+export const render = () => {
+  $timerCountdown.textContent = `${exportTime.min}:${exportTime.sec}`;
+};
+
+export const selectMode = () => {
   $timerMenu.addEventListener('click', e => {
     if (!e.target.matches('button')) return;
 
@@ -17,17 +27,25 @@ export default () => {
 
     e.target.classList.add('isActive');
 
-    state = e.target.textContent;
+    timerState.state = e.target.textContent;
 
-    if (state === 'Pomodoro') {
+    if (timerState.state === 'Pomodoro') {
       $body.style.backgroundColor = 'rgb(219, 82, 77)';
       $timerStart.style.color = 'rgb(219, 82, 77)';
-    } else if (state === 'Short Break') {
+      exportTime.min = time.pomodoroMin;
+      render();
+    } else if (timerState.state === 'Short Break') {
       $body.style.backgroundColor = 'rgb(70, 142, 145)';
       $timerStart.style.color = 'rgb(70, 142, 145)';
+      exportTime.min = time.shortBreakMin;
+      render();
     } else {
       $body.style.backgroundColor = 'rgb(67, 126, 168)';
       $timerStart.style.color = 'rgb(67, 126, 168)';
+      exportTime.min = time.longBreakMin;
+      render();
     }
   });
 };
+
+document.addEventListener('DOMContentLoaded', render());
