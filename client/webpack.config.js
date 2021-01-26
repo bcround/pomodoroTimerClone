bb060eeb0992be433df3ca7b7c8e60699ba6f2ad
jiwon <iamkjw77@naver.com>
2021-01-26 10:55:29 +0900

    feat: modified setting

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,21 +2,15 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  // entry file
-  // https://webpack.js.org/configuration/entry-context/#entry
-  entry: ['@babel/polyfill', './src/js/main.js', './src/sass/main.scss'],
-  // 번들링된 js 파일의 이름(filename)과 저장될 경로(path)를 지정
-  // https://webpack.js.org/configuration/output/#outputpath
-  // https://webpack.js.org/configuration/output/#outputfilename
+  entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: 'bundle.js',
+    publicPath: '/dist/'
   },
   plugins: [
-    // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
     new MiniCssExtractPlugin({ filename: 'style.css' })
   ],
-  // https://webpack.js.org/configuration/module
   module: {
     rules: [
       {
@@ -45,6 +39,9 @@ module.exports = {
     ]
   },
   devtool: 'source-map',
-  // https://webpack.js.org/configuration/mode
-  mode: 'development'
+  mode: 'development',
+  devServer: {
+    port: 8081,
+    publicPath: '/dist/'
+  }
 };
