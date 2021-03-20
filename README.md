9e28c0920b14f44d9fc7130d5867d286b3acd2c5
jiwon <iamkjw77@gmail.com>
2021-03-20 12:14:35 +0900

    docs: Add README.md

---
+++ README.md
@@ -0,0 +1,32 @@
+# ⏰뽀모도로, 클론 코딩⏰
+
+## ✨ 서비스 소개
+
+뽀모도로 기법이란 25분 동안 한가지 일에 전적으로 집중하고 5분 휴식하여 집중시간에는 인터넷 뉴스를 보거나 소셜 미디어에 한 눈 팔지 않고 오직 일에만 집중하는 기법이다.
+
+<img src="./client/src/images/pomodoro.gif">
+
+## 👨‍👨‍👧‍👦 함께한 사람들
+
+- [👨병국](https://github.com/bg-shorthand)
+- [👸지원](https://github.com/iamkjw77)
+- [🧑봉철](https://github.com/bcround)
+
+## 🎈 기술 스택
+
+![image](./client/src/images/skills.png)
+
+## 🔊 주요 기능
+
+##### <Task List>
+
+- Task 추가 및 삭제
+- Task 체크
+- Task 별 횟수 설정
+- 총 Task 종료 시간 표시
+
+##### <Timer>
+
+- 스탑 워치
+- 모드 전환
+- 시간 설정
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,46 +2,38 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/js/main.js',
+  entry: ['./src/js/main.js', './src/sass/main.scss'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/dist/'
+    publicPath: '/dist/',
   },
-  plugins: [
-    new MiniCssExtractPlugin({ filename: 'style.css' })
-  ],
+  plugins: [new MiniCssExtractPlugin({ filename: 'style.css' })],
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src/js')
-        ],
+        include: [path.resolve(__dirname, 'src/js')],
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties']
-          }
-        }
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
       },
       {
         test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ],
-        exclude: /node_modules/
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        exclude: /node_modules/,
+      },
+    ],
   },
   devtool: 'source-map',
   mode: 'development',
   devServer: {
     port: 8081,
-    publicPath: '/dist/'
-  }
+    publicPath: '/dist/',
+  },
 };
